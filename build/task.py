b77c5692622d833b63c4b5f4304bfe7968f336f8
Sanchit GARG <31209166+SANCHIT-GARG@users.noreply.github.com>
2022-06-28 08:50:14 +0800

    Update task.py

--- build/task.py
+++ build/task.py
@@ -6,46 +6,6 @@ import os
 import sys
 tfds.disable_progress_bar()
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--lr', dest='lr',
-#                     default=0.01, type=float,
-#                     help='Learning rate.')
-# parser.add_argument('--epochs', dest='epochs',
-#                     default=1, type=int,
-#                     help='Number of epochs.')
-# parser.add_argument('--steps', dest='steps',
-#                     default=200, type=int,
-#                     help='Number of steps per epoch.')
-# parser.add_argument('--distribute', dest='distribute', type=str, default='single',
-#                     help='distributed training strategy')
-# args = parser.parse_args()
-
-print('Python Version = {}'.format(sys.version))
-print('TensorFlow Version = {}'.format(tf.__version__))
-print('TF_CONFIG = {}'.format(os.environ.get('TF_CONFIG', 'Not found')))
-print('DEVICES', device_lib.list_local_devices())
-
-# Single Machine, single compute device
-# if args.distribute == 'single':
-#     if tf.test.is_gpu_available():
-#         strategy = tf.distribute.OneDeviceStrategy(device="/gpu:0")
-#     else:
-#         strategy = tf.distribute.OneDeviceStrategy(device="/cpu:0")
-# # Single Machine, multiple compute device
-# elif args.distribute == 'mirror':
-#     strategy = tf.distribute.MirroredStrategy()
-# # Multiple Machine, multiple compute device
-# elif args.distribute == 'multi':
-#     strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
-
-# Multi-worker configuration
-# print('num_replicas_in_sync = {}'.format(strategy.num_replicas_in_sync))
-
-# Preparing dataset
-# BUFFER_SIZE = 10000
-# BATCH_SIZE = 64
-
-
 ########## --------------------
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
@@ -106,24 +66,11 @@ def build_model():
     
     return model
 
-
-# MODEL_DIR = os.getenv("AIP_MODEL_DIR")
-
-# train_dataset = make_datasets_unbatched().batch(GLOBAL_BATCH_SIZE)
-
-# with strategy.scope():
-  # Creation of dataset, and model building/compiling need to be within
-  # `strategy.scope()`.
-    # model = build_and_compile_cnn_model()
 model = build_model()
 
 
 # The patience parameter is the amount of epochs to check for improvement
 early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
 model.fit(x = training_set, validation_data = test_set, epochs=1, callbacks=[early_stop])
-    
-##### model.fit(x=train_dataset, epochs=args.epochs, steps_per_epoch=args.steps)
-
-#model.save(MODEL_DIR)
 
 model.save('gs://sanchit909090-bucket/wcm/model')
